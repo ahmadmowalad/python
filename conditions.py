bdf806c3c9efe2bea626cf0626e36332b31ec11d
ahmad <ahmad@users-MacBook-Pro.local>
2020-06-03 14:36:54 +0300

    conditions

---
+++ conditions.py
@@ -0,0 +1,7 @@
+username=input("please insert your name : ")
+passwor=input("please insert your password : ")
+
+if username=='ahmad' and passwor=='123':
+    print('welcome to your email')
+else:
+        print('error')
