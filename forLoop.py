f1828f4447e18173cca01f76a56662f3b674db99
ahmad <ahmad@users-MacBook-Pro.local>
2020-06-04 11:23:04 +0300

    forLoop

---
+++ forLoop.py
@@ -0,0 +1,17 @@
+subject=['Arabic','english','math']
+print(subject)
+print(subject[0])
+subject.append('history')
+print(subject)
+print(subject[1:4])
+#subject.pop()
+#print(subject)
+#subject.pop()
+#print(subject)
+
+
+for x in subject:
+    print(x)
+
+
+
