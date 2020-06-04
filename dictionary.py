646de3416a06825cd56cd7abc90dabf59c128da7
ahmad <ahmad@users-MacBook-Pro.local>
2020-06-04 11:31:20 +0300

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+subject_with_no_student={"arabic":3,"english":3,"math":2}
+print(subject_with_no_student)
+
+student={"arabic":["ali","ahmad","mona"],"english":["thomas","aurther","jone"]}
+print(student)
+
+student["math"]=["salah","hussen"]
+print(student)
+
+student.update({"history":"ahmad"})
+print(student)
